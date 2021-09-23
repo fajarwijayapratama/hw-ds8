9a9450b4ad858059b3939057b5fed458654d2929
pannandayu <poligoncf22@gmail.com>
2021-09-23 10:38:55 +0700

    ganti nama

---
+++ schema_PannandaLikoYu.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    first_name varchar(255),
+    last_name varchar(255)
+    first_name varchar(30)
+    last_name varchar(30)
+);
+insert into users (username, first_name, last_name) values (pannanda_digital_skola, Pannanda_Liko_Yu)
